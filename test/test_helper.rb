64963154830493cd6b86145e1283637ae92ae917
Alex Lien <codefellow@aqlien.com>
2014-05-22 17:09:02 -0700

    begin adding ability to log in with username

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,3 +7,10 @@ require "minitest/rails/capybara"
 class ActiveSupport::TestCase
   fixtures :all
 end
+
+def sign_in(email = users(:sam).email, password = "password")
+  click_on I18n.t(:sign_in_link)
+  fill_in "Email", with: email
+  fill_in "Password", with: password
+  click_on "Sign in"
+end
