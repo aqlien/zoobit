7018a6a51224dca8758f1524ca0732e0773864f5
xdothackerx <xdothackerx@gmail.com>
2014-05-27 10:00:16 -0700

    Add title bar that changes per page, improved top-bar.

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,4 +1,5 @@
 class Dog < Pet
-    BREEDS = ["German Shepherd", "Welsh Corgi", "Golden Retriever", "Labrador Retriever", "Shiba Inu", "Husky",
-    "Beagle", "Bulldog", "Yorkshire Terrier", "Boxer", "Poodle", "Rottweiller"]
+  BREEDS = ["German Shepherd", "Welsh Corgi", "Golden Retriever", "Black Labrador Retriever",
+  "Yellow Labrador Retriever", "Chocolate Labrador Retriever", "Shiba Inu", "Husky",
+  "Beagle", "Bulldog", "Yorkshire Terrier", "Boxer", "Poodle", "Rottweiller", "Kick-Me Dog"]
 end
