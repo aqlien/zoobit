1e6e78b35768384d133d5803708681802bbb6c1e
Alex Lien <codefellow@aqlien.com>
2014-05-29 10:21:55 -0700

    add pet stories

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,5 +1,6 @@
 class Dog < Pet
   BREEDS = ["German Shepherd", "Welsh Corgi", "Golden Retriever", "Black Labrador Retriever",
   "Yellow Labrador Retriever", "Chocolate Labrador Retriever", "Shiba Inu", "Husky",
-  "Beagle", "Bulldog", "Yorkshire Terrier", "Boxer", "Poodle", "Rottweiller", "Kick-Me Dog"]
+  "Beagle", "Bulldog", "Yorkshire Terrier", "Boxer", "Poodle", "Rottweiller", "Greyhound",
+  "Pit Bull Terrier"]
 end
--- test/features/authentication/username_authentication_test.rb
+++ test/features/authentication/username_authentication_test.rb
@@ -6,8 +6,8 @@ feature "Basic Devise Authentication" do
     click_on I18n.t(:sign_up_link)
     fill_in "Username", with: "newbie" #Username parameter added, required
     fill_in "Email", with: "newbie@localhost.com"
-    fill_in "Password", with: "12341234"
-    fill_in "Password confirmation", with: "12341234"
+    fill_in "user_password", with: "12341234"
+    fill_in "user_password_confirmation", with: "12341234"
     click_on "Sign Up"
     page.must_have_content I18n.t("devise.registrations.signed_up")
   end
